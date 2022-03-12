65542ce086535da8aefb8eb1ed764a5eecbb9685
Eric Sohng <ericsohng@gmail.com>
2022-03-11 16:00:13 -0800

    complete algorithm for average-pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,31 @@
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+function averagePair(numArray, targetVal) {
+  let left = 0;
+  let right = numArray.length - 1;
+
+  while (left < right) {
+    let avg = (numArray[left] + numArray[right]) / 2;
+
+    if (avg === targetVal) {
+      return true;
+    } else if (avg < targetVal) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return false;
 }
+
+//naive solution is to pick first element, find average
+// with following elements until you find or dont find a pair
+
+// use multiple pointers
+// set left pointer and right pointer to beginning and
+//end of array
+
+// while left < right
+// avg = (left + right) / 2
+// check if avg is the target, return true
+// if avg is < target , left++
+// if avg is > target, right--
