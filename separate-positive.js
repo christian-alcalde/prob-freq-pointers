51a4fc04ec0161f49e654edb2bbcd1b21afe141d
Christian Alcalde <cc.alcalde@yahoo.com>
2022-03-11 17:09:39 -0800

    added docstring for separatePositive function

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,13 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function separatePositive() {
-}
+/** Takes an array and separates positive values to the
+ * left side and negative numbers to the right side in
+ * no particular order.
+ */
+function separatePositive() {}
+
+// naive:
+
+// [2, -1, -3, 6, -8, 10]
+// [2, 10, -3, 6, -8, -1]
+// [2, 10, 6, -3, -8, -1]
